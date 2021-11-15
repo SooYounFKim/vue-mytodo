515b9f3e2c522cd6da5eb77137cd9fb2587a4cd3
SooYounFKim <sooyoun.f.kim@gmail.com>
2021-11-15 16:54:40 +0900

    init

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `
+                    @import "@/assets/style/_mixins.scss";
+                    @import "@/assets/style/_variables.scss";
+                `
+            }
+        }
+    }
+}
